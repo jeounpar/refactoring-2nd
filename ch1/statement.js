8694be7c6460997767a9eef474a4f6abdbce6185
jeounpar <cdex6531@ajou.ac.kr>
2023-01-04 20:55:28 +0900

    add amoutFor function

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,28 +1,24 @@
-const plays = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as-like': { name: 'As You Like it', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
-};
-
-const invoices = [
-  {
-    customer: 'BigCo',
-    performances: [
-      {
-        playID: 'hamlet',
-        audience: 55,
-      },
-      {
-        playID: 'as-like',
-        audience: 35,
-      },
-      {
-        playID: 'othello',
-        audience: 40,
-      },
-    ],
-  },
-];
+function amoutFor(performance, play) {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy':
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
+      }
+      result += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return result;
+}
 
 function statement(invoices, plays) {
   let totalAmout = 0;
@@ -34,33 +30,18 @@ function statement(invoices, plays) {
     maximumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoices[0].performances) {
-    const play = plays[perf.playID];
-    let thisAmout = 0;
+  for (let performance of invoices[0].performances) {
+    const play = plays[performance.playID];
+    let thisAmout = amoutFor(performance, play);
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmout = 40000;
-        if (perf.audience > 30) {
-          thisAmout += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmout = 30000;
-        if (perf.audience > 20) {
-          thisAmout += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmout += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(performance.audience - 30, 0);
     if ('comedy' === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += Math.floor(performance.audience / 5);
     }
 
-    result += `${play.name}: ${format(thisAmout / 100)} (${perf.audience}석)\n`;
+    result += `${play.name}: ${format(thisAmout / 100)} (${
+      performance.audience
+    }석)\n`;
     totalAmout += thisAmout;
   }
   result += `총액: ${format(totalAmout / 100)}\n`;
